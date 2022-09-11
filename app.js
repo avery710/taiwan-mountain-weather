a174cd61372c25c8b72906477c1ab9f379563d9f
“avery710” <angellinbalabababa@gmail.com>
2022-09-11 14:17:38 +0800

    html done

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 const api = `https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization=CWB-F44E44EA-6305-47FE-A4E2-CB8AC0A12E30&format=JSON&stationId=C0F0A0,C0F0E0,C0H9C0,C0R600,C0I530,C0S750,467530&elementName=`;
 
 fetch(api) // promise based object
